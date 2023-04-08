b0d01e1666da5eaedeeded571ef678215810c705
Tom Duchambon <tom.duchambon@gmail.com>
2023-04-08 15:53:39 +0100

    middle-bulb to home path when /about & /contact

--- src/components/Lightbulbs/index.js
+++ src/components/Lightbulbs/index.js
@@ -1,13 +1,13 @@
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb} from '@fortawesome/free-solid-svg-icons'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 
 const Lightbulbs = () => {
 
   const [isYellow, setIsYellow] = useState(false);
-
+  const location = useLocation();
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsYellow(true);
@@ -20,9 +20,15 @@ const Lightbulbs = () => {
         <NavLink exact="true" activeclassname="active" to="/projects">
         <FontAwesomeIcon icon={faLightbulb} className={`lightbulb left-bulb ${isYellow ? 'yellow-left' : ''}`}/>
         </NavLink>
-        <NavLink exact="true" activeclassname="active" to="/about">
-        <FontAwesomeIcon icon={faLightbulb} className={`lightbulb middle-bulb ${isYellow ? 'yellow-mid' : ''}`} />
-        </NavLink>
+        {location.pathname === "/about" || location.pathname === "/contact" ? (
+          <NavLink exact activeClassName="active" to="/home">
+            <FontAwesomeIcon icon={faLightbulb} className={`lightbulb middle-bulb ${isYellow ? "yellow-mid" : ""}`} />
+          </NavLink>
+        ) : (
+          <NavLink exact activeClassName="active" to="/about">
+            <FontAwesomeIcon icon={faLightbulb} className={`lightbulb middle-bulb ${isYellow ? "yellow-mid" : ""}`} />
+          </NavLink>
+        )}
         <NavLink exact="true" activeclassname="active" to="/contact">
         <FontAwesomeIcon icon={faLightbulb} className={`lightbulb right-bulb ${isYellow ? 'yellow-right' : ''}`} />
         </NavLink>
